4d1cb9a20ee48228bd86024a351fa1e3abe854ed
Jeong-ki <7863z@naver.com>
2024-12-19 09:16:56 +0900

    feat: header 레이아웃 구현

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -7,10 +7,14 @@ import { Button } from '@/components/common/button';
 import cn from 'classnames';
 import { useOnClickOutside } from '@/hooks/useOnClickOutside';
 import SearchIcon from '@img/icon/search.svg';
+import useGlobalStore from '@/store/global';
+import { isTokenValid } from '@/utils/common';
 
 export const Header = () => {
-  const profileRef = useRef(null);
+  const { accessToken } = useGlobalStore();
+  const isLoggedIn = !!accessToken && isTokenValid(accessToken);
   const [isOpenProfile, setIsOpenProfile] = useState<boolean>(false);
+  const profileRef = useRef(null);
 
   const handleOpenProfile = () => {
     setIsOpenProfile((prev) => !prev);
@@ -22,10 +26,9 @@ export const Header = () => {
     <header className="doc-header">
       <h1 className="doc-title">
         <Link href="/" className="link_logo">
-          WatchBit
+          BitWatch
         </Link>
       </h1>
-
       <nav className="nav-header">
         <ul>
           <li>
@@ -49,53 +52,54 @@ export const Header = () => {
           <span>/</span> <p>를 눌러 검색해보세요.</p>
         </Button>
       </nav>
-
       <div>
-        <Button color="primary" size="medium">
-          로그인
-        </Button>
-        <div ref={profileRef} className={cn('info_my', { layer_open: isOpenProfile })}>
-          <div className="profile_user">
-            <strong className="screen_out">사용자 프로필</strong>
-            <button className="link_thumb" type="button" onClick={handleOpenProfile}>
-              <div className="wrap_thumb">
-                <Image
-                  src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg"
-                  width={35}
-                  height={35}
-                  alt="사용자 이미지"
-                />
-              </div>
-            </button>
-          </div>
-
-          <div className="profile_layer">
-            <div className="inner_layer">
-              <div className="layer_body">
-                <div className="info_profile">
-                  <strong className="screen_out">로그인 정보</strong>
-                  <div className="wrap_thumb">
-                    <Image
-                      src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg"
-                      width={35}
-                      height={35}
-                      alt="사용자 이미지"
-                    />
-                  </div>
-                  <div className="info_thumb">
-                    <strong className="tit_name">유저명</strong>
-                    <span className="txt_id">이메일</span>
+        {isLoggedIn ? (
+          <div ref={profileRef} className={cn('info_my', { layer_open: isOpenProfile })}>
+            <div className="profile_user">
+              <strong className="screen_out">사용자 프로필</strong>
+              <button className="link_thumb" type="button" onClick={handleOpenProfile}>
+                <div className="wrap_thumb">
+                  <Image
+                    src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg"
+                    width={35}
+                    height={35}
+                    alt="사용자 이미지"
+                  />
+                </div>
+              </button>
+            </div>
+            <div className="profile_layer">
+              <div className="inner_layer">
+                <div className="layer_body">
+                  <div className="info_profile">
+                    <strong className="screen_out">로그인 정보</strong>
+                    <div className="wrap_thumb">
+                      <Image
+                        src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg"
+                        width={35}
+                        height={35}
+                        alt="사용자 이미지"
+                      />
+                    </div>
+                    <div className="info_thumb">
+                      <strong className="tit_name">유저명</strong>
+                      <span className="txt_id">이메일</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="layer_foot">
-                <button type="button" className="btn_txt">
-                  로그아웃
-                </button>
+                <div className="layer_foot">
+                  <button type="button" className="btn_txt">
+                    로그아웃
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <Button color="primary" size="medium">
+            로그인
+          </Button>
+        )}
       </div>
     </header>
   );
--- src/utils/common/index.ts
+++ src/utils/common/index.ts
@@ -10,3 +10,12 @@ export const isDev = process.env.NODE_ENV === 'development' || process.env.NODE_
 export const isEmptyNum = (value: undefined | null | number) => {
   return value === undefined || value === null || Number.isNaN(value);
 };
+
+export const isTokenValid = (token: string) => {
+  try {
+    const { exp } = JSON.parse(atob(token.split('.')[1]));
+    return Date.now() / 1000 < exp;
+  } catch {
+    return false;
+  }
+};
